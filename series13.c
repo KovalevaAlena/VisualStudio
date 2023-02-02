a8dd8ecd4419be72c030e63862d5c5a29c00a1fd
LaduBug <118794792+KovalevaAlena@users.noreply.github.com>
2023-02-02 11:22:04 +0300

    Add files via upload

---
+++ series13.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int n, i, a, max = 0;
+    scanf("%d", &n);
+    int s[n];
+    for(i = 0;i < n; i++)
+    {
+        scanf("%d", &a);
+        if (a == 0)
+        break;
+    }    
+
+    return 0;
+}
